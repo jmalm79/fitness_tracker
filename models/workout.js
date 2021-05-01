cd2651306811e8d6f49190464a7fe70bb4ad66c4
Josh A <jmalm79@gmail.com>
2021-04-30 20:43:11 -0500

    updated file structures, updated code to flow through server file, instead of through multiple files

--- models/workout.js
+++ models/workout.js
@@ -5,31 +5,43 @@ const Schema = mongoose.Schema;
 const WorkoutSchema = new Schema({
     day: {
         type: Date,
-        default: Date.now
+        default: Date.now()
     },
-    exercise: {
-        type: String,
-    },
-    name: {
-        type: String,
-        trim: true,
-    },
-    duration: {
-        type: Number,
-        trim: true,
-    },
-    weight: {
-        type: Number,
-    },
-    reps: {
-        type: Number,
-    },
-    sets: {
-        type: Number,
+    exercises: [
+        {
+        type: {
+            type: String,
+            required: "Exercise type is required",
+            trim: true
+        },
+        name: {
+            type: String,
+            required: "Name is required",
+            trim: true
+        },
+        duration: {
+            type: Number, 
+            required: "Duration is required"
+        },
+        weight: {
+            type: Number,
+            required: "Weight is required"
+        }, 
+        reps: {
+            type: Number, 
+            required: "Reps are required"
+        }, 
+        sets: {
+            type: Number, 
+            required: "Sets are required"
+        }, 
+        distance: {
+            type: Number
+        }
     }
+]
+}) 
 
-}); 
-
-const Workout = mongoose.model("Workout", workoutSchema);
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const path = require("path");
 const PORT = process.env.PORT || 3000
-
-const app = express();
 
 const db = require("./models");
 
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,17 +14,67 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
-  useCreateIndex: true,
+  useCreateIndex: true  
+});
+
+
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get ('/exercise', (req, res) => {
+res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+
+app.get ('/stats', (req, res) => {
+res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+
+app.get('/api/workouts', (req, res) => {
+  db.Workout.aggregate([
+      {
+          $addFields:{
+              totalDuration: {$sum: "$exercise.duration"}
+          }
+      }
+  ])
+  .then(dbWorkout => {
+      res.json(dbWorkout);
+  }).catch(err => {
+      res.json(err);
   });
 
-  app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
-  
-  db.on("error", error => {
-    console.log("Database Error:", error);
-  });
+});
 
-app.get("/", (req, res) => {
-  res.send(index.html);
+app.post("/api/workouts/", ({body}, res) => {
+  db.Workout.create(body)
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  }).catch(err => {
+    res.json(err);
+  });
+});
+
+app.put("/api/workouts/:id", (req, res) => {
+  id = req.params.id
+  db.Workout.findOneAndUpdate({"_id": id}, {$push: {exercise: req.body} })
+  .then(result => {
+    res.json(result);
+    })
+  .catch(err => {
+    res.json(err);
+  })
+})
+
+app.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  }) .catch(err => {
+    res.json(err);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App Running! ${PORT}!`)
 });
